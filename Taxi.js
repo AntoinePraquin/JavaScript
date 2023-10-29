c42da2d55f2c69cc420bdcb26decc891e68a207e
AntoinePraquin <149180780+AntoinePraquin@users.noreply.github.com>
2023-10-29 15:41:22 +0100

    Add files via upload
    Bonjour, j'ai fait se qui me semblais etre le plus proche de ce que demander mais je n'arrive pas a comprendre comment mettre mes fonction et les methode en action ensemble.
    contrairement au pokemon ou j'ai pus demander conseil, je cherche sur les site de js pour plus d'info mais ce sont souvent des syntax que je n'arrive pas a organiser.

---
+++ Taxi.js
@@ -0,0 +1,39 @@
+class Personnage {
+    constructor(name,Mental,nbTaxi) {
+        this.Mental = Mental
+        this.name = name
+        this.nbTaxi = nbTaxi
+    }
+}
+
+class Musique {
+    constructor(nameM) {
+    this.nameM = nameM
+}
+    changetaxi(){
+
+        if (Musique == [0]) {
+            Mental = (Mental - 1);
+            nbTaxi = (nbTaxi + 1);
+            Feurouge = (Feurouge - 1)
+                return true;
+            }
+    
+            return false;
+        }
+}
+
+let John = new Personnage(John,10,0);
+let Musique = ["Annissa", "VetV", "Ricadendoti", "VinItalien", "Hummanoide"];
+let Feurouge = 30;
+
+while(Mental > 0 & this.changetaxi = true) {
+
+    John.changetaxi("John prend un autre taxi, sacré Annissa !")
+
+};
+
+if(Mental <= 0){
+    alert("explosion")
+};
+
